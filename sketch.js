3451e97027a0b9f973c4059d7ee288ea9ffc8381
Trent <51332495+trent0@users.noreply.github.com>
2024-01-30 19:46:54 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(400, 400);
+  pixelDensity(22);
+  
+}
+
+function draw() {
+ 
+  noStroke();
+  fill(220, -5, 0, 2);;
+circle(95, 65, 80);
+noStroke();
+fill(0, 225, -5, 3);
+circle(125, 105, 80);
+noStroke();
+fill(0, -5, 225, 2);  
+circle(75, 105, 80);
+
+}
